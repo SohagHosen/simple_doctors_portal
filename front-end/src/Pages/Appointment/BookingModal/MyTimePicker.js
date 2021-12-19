49a7a8b72a3e3c81ce9dc0c50754a748ee4be270
sohaghosen <sohag.hosen@outlook.com>
2021-12-19 16:53:08 +0600

    update

---
+++ front-end/src/Pages/Appointment/BookingModal/MyTimePicker.js
@@ -0,0 +1,37 @@
+import * as React from "react";
+import TextField from "@mui/material/TextField";
+import AdapterDateFns from "@mui/lab/AdapterDateFns";
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import TimePicker from "@mui/lab/TimePicker";
+
+export default function MyTimePicker({ time, setTime, setAvailableTime }) {
+  const handleChange = (newValue) => {
+    setTime(newValue);
+    fetch(`http://localhost:5000/checkAvailableTime/${time}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setAvailableTime(data.message);
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  return (
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <TimePicker
+        value={time}
+        onChange={handleChange}
+        // onChange={(newValue) => setTime(newValue)}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            onChange={handleChange}
+            sx={{ width: "90%", m: 1 }}
+            required
+          />
+        )}
+      />
+    </LocalizationProvider>
+  );
+}
